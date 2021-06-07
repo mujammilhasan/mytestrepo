c1b676b419d83550afc1a9115fd21b4949d31ab3
jamil <jamil@jamil-PC>
2021-06-07 06:30:45 +0530

    [java coding] added new java file

---
+++ TextProject/src/com/test/Test.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("commit first project");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
